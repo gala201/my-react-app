99552ad2e910807f3115788dfc2825e991178912
programer10 <programer10@ucione.local>
2022-03-10 20:27:55 +0100

    hooks

--- src/components/playground/Playground.js
+++ src/components/playground/Playground.js
@@ -15,13 +15,87 @@ export default function Playground() {
         } */
 
     const [name, setName] = useState("")
+    const [adresa, setAdresa] = useState("")
+    const [phone, setPhone] = useState("")
+
+    const [product, setProduct] = useState({
+        name: "Skecers tenisice",
+        description: "lorem ipsum sit",
+        price: 120
+    })
+
+    const [vozac, setVozac] = useState({
+        ime: "",
+        prezime: "",
+        adresa: ""
+    })
+
+
+
+
+    console.log(product.name);
+    console.log(product["name"]);
+
+
+    const handleUpdate = (e) => {
+        const value = e.target.value
+        const name = e.target.name
+
+
+        setProduct(prevValues => ({
+            ...prevValues, ["name"]: value
+        }))
+
+
+    }
+
+    const updatePrice = (e) => {
+        const price = e.target.value
+
+        setProduct(prevValues => ({
+            ...prevValues, price: price
+        }))
+    }
+
+    const updateId = (e) => {
+        const value = e.target.value
+        const name = e.target.name
+
+
+
+        setVozac(prevValues => ({
+            ...prevValues, [name]: value
+        }))
+
+    }
+
 
     return (
         <>
             <h1>My Playground</h1>
+
+            {/*
+            <h3>Vase ime i prezime je {name}</h3><br />
+            <h4>Vasa adresa je {adresa}</h4><br />
+            <h4>Vas broj telefona je {phone}</h4> */}
+
+            <small>Naziv proizvoda - </small>
+            <span>{product.name}</span> <br />
+            <small>Opis proizvoda - </small>
+            <span>{product.description}</span><br />
+            <small>Cijena - </small>
+            <span><b>{product.price}</b></span>
             <hr />
 
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Upiši ime i prezime" /><br />
+            <div>
+                <label htmlFor="">Naziv proizvoda</label> <br />
+                <input name="name" onChange={(e) => handleUpdate(e)} type="text" value={product.name} /> <br /> <br />
+                <label htmlFor="">Opis proizvoda</label><br />
+                <input type="text" value={product.description} /><br />
+                <label htmlFor="">Cijena</label><br />
+                <input type="text" value={product.price} name="price" onChange={(e) => updatePrice(e)} />
+            </div><hr />
+
 
 
             <hr />
@@ -30,7 +104,29 @@ export default function Playground() {
             <button onClick={() => setCounter(Math.max(1, counter - 1))}>Umanji</button>
 
             <hr />
-            <Sandbox price={donutPrice} quantity={counter} cipela={discount} name={name} />
+            <div>
+                <span>{vozac.ime}</span><br />
+                <span>{vozac.prezime}</span><br />
+                <span>{vozac.adresa}</span>
+            </div><hr />
+            <div>
+                <input type="text" value={vozac.ime} name="ime" onChange={(e) => updateId(e)} /> <br />
+                <input type="text" value={vozac.prezime} name="prezime" onChange={(e) => updateId(e)} /> <br />
+                <input type="text" value={vozac.adresa} name="adresa" onChange={(e) => updateId(e)} />
+            </div>
+            <Sandbox
+                price={donutPrice}
+                quantity={counter}
+                risetajKaunter={setCounter}
+                cipela={discount}
+                name={name}
+                adresa={adresa}
+                phone={phone}
+                postaviAdresu={setAdresa}
+                postaviIme={setName}
+                postaviBroj={setPhone}
+
+            />
         </>
 
     )
--- src/components/playground/Sandbox.js
+++ src/components/playground/Sandbox.js
@@ -1,19 +1,34 @@
 export default function Sandbox(props) {
 
-    const totalPrice = props.price * props.quantity
-    /*     const priceDiscount = props.cipela
-    
-        if(props.quantity >= 5) {
-            totalPrice = totalPrice - totalPrice/100*priceDiscount
-        } */
+    /* let totalPrice = props.price * props.quantity
+    const priceDiscount = props.cipela
 
+    if (props.quantity >= 5) {
+        totalPrice = totalPrice - totalPrice / 100 * priceDiscount
+    }
 
+    if (props.quantity > 7) {
+        props.risetajKaunter(1)
+    } */
 
 
     return (
-        <>
-            <h2>Cijena ovih proizvoda je: {props.quantity < 5 ? totalPrice : totalPrice - (totalPrice * .10)}</h2>
-            <h3>Vaše Ime i prezime je {props.name}</h3>
-        </>
+        <div>
+            {/*             <h2>Cijena ovih proizvoda je: {totalPrice}</h2>
+
+            <input type="text" value={props.name} onChange={(e) => props.postaviIme(e.target.value)} placeholder="Upiši ime i prezime" /> <br />
+
+            <input type="text" value={props.adresa} onChange={(e) => props.postaviAdresu(e.target.value)} placeholder="Upiši adresu" /> <br />
+
+            <input type="text" value={props.phone} onChange={(e) => props.postaviBroj(e.target.value)} placeholder="Upiši broj telefona" /> */}
+
+
+
+
+
+
+        </div>
+
+
     )
 }
